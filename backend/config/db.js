584e1caf75fe283dc055f54b63a892223ca9c572
ecotelDev <dev@ecotel.com.vn>
2026-07-20 10:49:49 +0700

    [fix] dừng server khi db disconnect, docker db thêm key file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connect=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err)
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (err) {
+    console.error("❌ Error connecting to MongoDB:", err.message);
+    process.exit(1); // Dừng server nếu không kết nối được DB
+  }
+};
 
-module.exports=connect
+module.exports = connect;
